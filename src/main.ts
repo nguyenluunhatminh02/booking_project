b76723d55ddddcf34df9485a4e1e05a235be55fe
kronos202 <kronosss2002@gmail.com>
2025-08-28 14:53:03 +0700

    build(db): add model and minimize

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('trust proxy', 1); // trust proxy đầu tiên (Nginx)
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
